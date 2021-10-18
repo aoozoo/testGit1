4757ef2682a7f640847c2f3d62021c372bc51d15
aoozoo <aoozoo@outlook.com>
2021-10-19 02:57:04 +0800

    aaa gengxin

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 [root@VmJasonPan_10.10.10.11 /data/reset]# git add reset1.txt
-[root@VmJasonPan_10.10.10.11 /data/reset]# echo 'reset' >reset.txt
+[root@VmJasonPan_10.10.10.11 /data/reset]# echo 'reaaaaset' >reset.txt
 [root@VmJasonPan_10.10.10.11 /data/reset]# git status
 # On branch master
 # Changes to be committed:
@@ -11,9 +11,5 @@
 #   (use "git add <file>..." to include in what will be committed)
 #
 #	reset.txt
-[root@VmJasonPan_10.10.10.11 /data/reset]# git log --pretty=format:"%h - %an, %ar : %s" --graph
-* da97fd9 - aoozoo, 7 minutes ago : B
-* de2f541 - aoozoo, 7 minutes ago : A
-[root@VmJasonPan_10.10.10.11 /data/reset]# git reset --soft de2f541
-[root@VmJasonPan_10.10.10.11 /data/reset]# git status
+
 aaa
